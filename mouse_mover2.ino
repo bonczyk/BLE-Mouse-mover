244770126aa70b5abbb84463567fe2107f99f02e
Maciej <39693718+bonczyk@users.noreply.github.com>
2025-01-15 16:30:22 +0000

    Update mouse_mover2.ino

--- mouse_mover2.ino
+++ mouse_mover2.ino
@@ -1,6 +1,4 @@
 #include <BleMouse.h>
-#include <esp_int_wdt.h>
-#include <esp_task_wdt.h>
 
 BleMouse bleMouse("BMouse", "MB", 100);
 unsigned long lastMove = 0;
@@ -18,12 +16,12 @@ void setup() {
 void loop() {
   if(bleMouse.isConnected()) {
     unsigned long currentMillis = millis();
-    //if(currentMillis - lastMove >= interval - 2000) { Serial.println("Move in 2 sec ..");  }
+    if(currentMillis - lastMove >= interval - 2000) { Serial.println("Move in 2 sec ..");  }
     if(currentMillis - lastMove >= interval) {
      if (bleMouse.isConnected()) {
       int duration = random(50,200) + 1;
-      int randx = random(5) - 1;
-      int randy = random(5) - 1;
+      int randx = random(5) - 2;
+      int randy = random(5) - 2;
       Serial.println("Random numbers : X = "+String(randx)+" Y = "+String(randy) );
       moveMouse(-randx,-randy,duration);
       moveMouse(randx,randy,duration);
@@ -33,12 +31,7 @@ void loop() {
      }
     }  
   }
-}
-
-void hard_restart() { 
- esp_task_wdt_init(1,true); 
- esp_task_wdt_add(NULL);
- while(true);
+  delay(1000);
 }
 
 void moveMouse(int x, int y, int duration) { 
