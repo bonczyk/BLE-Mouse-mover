5968f5f2aff27bf376d55faa9465b91cd246936d
Maciej <39693718+bonczyk@users.noreply.github.com>
2025-01-31 15:19:12 +0100

    Update BMouse_noDelay.ino

--- BMouse_noDelay.ino
+++ BMouse_noDelay.ino
@@ -1,18 +1,15 @@
 #include <BleMouse.h>
 
-const byte LedPin = 8;
 BleMouse bleMouse("BMouse", "MB", 69);
-unsigned long lastMove = 0;
-unsigned long lastBlink = 0;
-unsigned long interval = 5000; //random(40,58) * 1000; 
-unsigned long AbsStartTime = millis(); 
-unsigned long currentMillis = millis();
-byte in2 = 0;
-byte mmove = 0;
-  unsigned long startTime = 0;
-  int duration = 0;
-        int x = 0;
-        int y = 0;
+
+const byte LedPin = 8;
+unsigned long lastMove = 0, lastBlink = 0, lastConn = 0 , startTime = 0; 
+unsigned long currentMillis = millis(), AbsStartTime = currentMillis; 
+unsigned long interval = 5000; //random(40,58) * 1000;
+int duration = 0, x = 0, y = 0;
+bool connected = false;
+byte in2 = 0, mmove = 0;
+
 
 void setup() {
   Serial.begin(115200);
@@ -29,93 +26,67 @@ void loop() {
   }
 }
 
-
 void switchLed() {
   digitalWrite(LedPin, !digitalRead(LedPin));
 }
 
-void every(int interv, void (*function)(),unsigned long &variable ){
+void every(int interv, void (*function)(),unsigned long &variable, bool reset){
   unsigned long cMillis = millis();
   if(cMillis - variable >= interv ) {
      (*function)();
-     variable = cMillis;
+     if (reset) { variable = cMillis; }
   }
 }
 
+void moveMouse(int x,int y) {
+  bleMouse.move(x,y);
+  Serial.println("X = "+String(x)+" Y = "+String(y) );
+}
+
+void in2sec() {
+  if(in2 == 0) { digitalWrite(LedPin, LOW); Serial.println("Move in 2 sec .."); in2++; }
+}
+
+void in1sec() {
+  if(in2 == 1) { digitalWrite(LedPin, HIGH); Serial.println("Move in 1 sec .."); in2++; }
+}
+
+void move() {
+  if (mmove == 0) { 
+    digitalWrite(LedPin, LOW); 
+    interval = random(40,58) * 1000;
+    duration = (random(3) * 100) + 100;
+    x = random(5) - 2;
+    y = random(5) - 2;         
+    Serial.println("Moving mouse : X = "+String(x)+" Y = "+String(y)+" Dur = "+String(duration)+" Interval = "+String(interval) );
+    mmove++; 
+    startTime = millis();
+  }
+  if (mmove == 1) {
+    if (bleMouse.isConnected()) {
+      if(millis()<startTime+duration) { moveMouse(x,y); }
+        else if(millis()<startTime+duration*2) { moveMouse(-x,-y); } 
+        else { lastMove = currentMillis; mmove=0;
+          digitalWrite(LedPin, HIGH); in2=0;
+        }
+    }
+  }  
+}
+void restartESP(){
+  ESP.restart();
+}
 
 void handle_mouse() {
   currentMillis = millis();
   if(bleMouse.isConnected()) {
     digitalWrite(LedPin, HIGH);    
-    if(currentMillis - lastMove >= interval - 2000) { if(in2 == 0) { digitalWrite(LedPin, LOW); Serial.println("Move in 2 sec .."); in2++; } }
-    if(currentMillis - lastMove >= interval - 1000) { if(in2 == 1) { digitalWrite(LedPin, HIGH); Serial.println("Move in 1 sec .."); in2++; } }
-    if(currentMillis - lastMove >= interval) { 
-      if (mmove == 0) { 
-        digitalWrite(LedPin, LOW); 
-        duration = (random(3) * 100) + 100;
-        x = random(5) - 2;
-        y = random(5) - 2;
-        Serial.println("Moving mouse : X = "+String(x)+" Y = "+String(y)+" Dur = "+String(duration) );
-        mmove++; 
-        startTime = millis();
-        
-        interval = random(40,58) * 1000;
-        Serial.println("Interval = "+String(interval)+"");
-        
-      }
-    }
-    if (mmove == 1) {
-      if (bleMouse.isConnected()) {
-         if(millis()<startTime+duration) {
-           bleMouse.move(x,y);
-           Serial.println("X = "+String(x)+" Y = "+String(y) );
-         }
-         else if(millis()<startTime+duration*2) {
-           bleMouse.move(-x,-y);
-           Serial.println("X = "+String(-x)+" Y = "+String(-y) );
-         } 
-         else { lastMove = currentMillis; mmove=0;
-           digitalWrite(LedPin, HIGH); in2=0;
-           Serial.println("END");
-         }
-      }
-    }  
+    connected = true;
+    every(interval - 2000,in2sec,lastMove,0);
+    every(interval - 1000,in1sec,lastMove,0);
+    every(interval,move,lastMove,1);
   } else { 
-     //if(currentMillis - lastBlink >= 500 ) { 
-     // digitalWrite(LedPin, !digitalRead(LedPin));
-     // lastBlink = currentMillis;
-     every(500,switchLed,lastBlink);
+    if (connected) { lastConn = currentMillis;  connected = false; }
+    every(500,switchLed,lastBlink,1);
+    every(15000,restartESP,lastConn,1);
   }
-}
-
-
-
-void moveMouse(int x, int y, int duration) { 
- 
-}
-
-// need forward declarations here
-void function2();
-void function1(void (*)());
-
-
-void loop1() {
-    function1(function2);
-}
-
-
-void function2() {
-    //do something
-}
-
-void setup1()
-{
-  Serial.begin(9600);
-  byte y = 0x35;
-  byte *ptr;
-  ptr = &y;     //& (ampersand sign) is used to get the address of RAM location that holds y
-  Serial.println(y, HEX); //shows: 35
-  //---------------------------
-  *ptr = 0x45;  //store 0x45 into a RAM location whose address is in ptr
-  Serial.println(y, HEX); //shows: 45
 }
